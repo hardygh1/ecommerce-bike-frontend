449c1b874cf17a5029c34ea219a6cdf6658b78a5
Bruno Dev <brunoguizado15@gmail.com>
2024-01-23 21:13:18 -0500

    Added Nav and Carousel

---
+++ src/customer/components/HomeCarosel/MainCarouselData.js
@@ -0,0 +1,15 @@
+export const mainCarouselData = [
+    {
+        image: "/PROM1.png",
+        path: "/PROM1.png"
+    },
+    {
+        image: "/PROM2.png",
+        path: "/PROM2.png"
+    },
+    {
+        image: "/PROM3.png",
+        path: "/PROM3.png"
+    },
+
+]
---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+
+import React from "react"
+import MainCarousel from "../../components/HomeCarosel/MainCarousel"
+
+const HomePage = () => {
+    return (
+        <div>
+            <MainCarousel />
+        </div>
+    )
+}
+
+export default HomePage
